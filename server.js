2d86cdd7dc53a3e5778b0f325ea496a5be0f457b
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-09-28 14:12:39 -0400

    new

--- server.js
+++ server.js
@@ -53,8 +53,8 @@ app.post("/imageurl", (req, res) => {
 
 // Load hash from your password DB.
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
 
 /*
